499eaf7fa6a13b04c6f6773472360419d2bf3990
zijiawen <zijialoveplay@qq.com>
2025-07-23 16:17:14 +0800

    feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import './App.css'
-import viteLogo from '/vite.svg'
+import './App.css';
+import viteLogo from '/vite.svg';
 
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img alt="Vite logo" className="logo" src={viteLogo} />
+        <a
+          href='https://vite.dev'
+          target='_blank'>
+          <img
+            alt='Vite logo'
+            className='logo'
+            src={viteLogo}
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img alt="React logo" className="logo react" src={reactLogo} />
+        <a
+          href='https://react.dev'
+          target='_blank'>
+          <img
+            alt='React logo'
+            className='logo react'
+            src={reactLogo}
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
-import react from '@vitejs/plugin-react'
-
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
+    },
   },
-    server: {
+  server: {
     port: 3000,
   },
-})
+});
